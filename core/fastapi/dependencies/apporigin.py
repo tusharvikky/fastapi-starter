6b06231a24c3b2d2656cda39431562f03df7197d
Tushar Deo <tusharvikky@gmail.com>
2021-09-03 04:26:56 +0530

    Update apporigin.py
    Request has tenant

--- core/fastapi/dependencies/apporigin.py
+++ core/fastapi/dependencies/apporigin.py
@@ -4,11 +4,12 @@ from app.models import Team
 
 
 class AppOrigin:
-    def __init__(self, app_origin: int = Header(None)):
+    def __init__(self, request: Request, app_origin: int = Header(None)):
 
         if not app_origin:
             raise HTTPException(status_code=422, detail="App-Origin header invalid")
 
         tenant = session.query(Team).filter(Team.id==app_origin).first()
+        request.tenant = tenant
         session.info['tenant'] = tenant
         
